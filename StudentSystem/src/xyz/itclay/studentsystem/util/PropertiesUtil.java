a03f330bb49d854b26c851594c89623df367991f
zsmiao <zsmiao2020@outlook.com>
2020-11-29 21:18:33 +0800

    完成简单的增删改查

--- StudentSystem/src/xyz/itclay/studentsystem/util/PropertiesUtil.java
+++ StudentSystem/src/xyz/itclay/studentsystem/util/PropertiesUtil.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class PropertiesUtil {
     public static Properties drive() throws Exception{
-        File file=new File("util/database.properties");
+        File file=new File("E:\\IdeaProject\\StudentSystem\\StudentSystem\\src\\xyz\\itclay\\studentsystem\\util\\database.properties");
         FileInputStream fileInputStream=new FileInputStream(file);
         Properties properties=new Properties();
         properties.load(fileInputStream);
--- StudentSystem/src/xyz/itclay/studentsystem/util/Student_jdbc.java
+++ StudentSystem/src/xyz/itclay/studentsystem/util/Student_jdbc.java
@@ -3,6 +3,7 @@ package xyz.itclay.studentsystem.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 /**
@@ -14,7 +15,12 @@ public class Student_jdbc {
     /**
      * 定义SQL语句
      */
-    private static final String ADD_STUDENT = "insert  into StudentSystem(Student_Id,Student_Name,Student_Sex,Grade,Classe,Major_Name,Department_Name) values (?,?,?,?,?,?,?)";
+    private static final String ADD_STUDENT = "insert  into tb_Student(Student_Id,Student_Name,Student_Sex,Grade,Classe,Major_Name,Department_Name) values (?,?,?,?,?,?,?)";
+    private static final String DELETE_STUDENT = "delete from tb_Student where Student_Id=?";
+    private static final String UPDATE_STUDENT = "update tb_Student set Student_Name=?,Student_Sex=?,Grade=?,Classe=?,Major_Name=?,Department_Name=? where Student_Id=?";
+    private static final String SHOW_STUDENT = " select  * from  tb_Student WHERE stu_id=?";
+    private static final String SHOWALL_STUDENT = "select * from tb_Student;";
+
     /**
      * 连接数据库的方法
      */
@@ -50,24 +56,104 @@ public class Student_jdbc {
         System.out.print("请输入要添加学生的学校>>>>>");
         String stuDepart = scanner.next();
         //通过SQL语句实现插入操作
-        PreparedStatement ps=conn.prepareStatement(ADD_STUDENT);
-        ps.setString(1,stuId);
-        ps.setString(2,stuName);
-        ps.setString(3,stuSex);
-        ps.setInt(4,stuGrade);
-        ps.setInt(5,stuClass);
-        ps.setString(6,stuMajor);
-        ps.setString(7,stuDepart);
-        if(ps.executeUpdate()>0){
+        PreparedStatement ps = conn.prepareStatement(ADD_STUDENT);
+        ps.setString(1, stuId);
+        ps.setString(2, stuName);
+        ps.setString(3, stuSex);
+        ps.setInt(4, stuGrade);
+        ps.setInt(5, stuClass);
+        ps.setString(6, stuMajor);
+        ps.setString(7, stuDepart);
+        if (ps.executeUpdate() > 0) {
             System.out.println("添加成功！");
-        }else {
+        } else {
             System.out.println("添加失败！");
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        Student_jdbc.connectionDatabase();
-        Student_jdbc.add();
-        conn.close();
+    /**
+     * 删除学生信息
+     */
+    public static void remove() throws Exception {
+        System.out.println("**********删除学生信息***********");
+        System.out.print("请输入要删除学生的学号>>>>>");
+        String stuID = scanner.next();
+        PreparedStatement ps = conn.prepareStatement(DELETE_STUDENT);
+        ps.setString(1, stuID);
+        if (ps.executeUpdate() > 0) {
+            System.out.println("删除成功！");
+        } else {
+            System.out.println("删除失败！");
+        }
+    }
+
+    /**
+     * 更改学生信息
+     */
+    public static void updata() throws Exception {
+        System.out.println("**********更改学生信息***********");
+        System.out.println("请输入要更改学生的学号>>>>>");
+        String stuId = scanner.next();
+        System.out.print("请输入要更改学生的姓名>>>>>");
+        String stuName = scanner.next();
+        System.out.print("请输入要更改学生的性别>>>>>");
+        String stuSex = scanner.next();
+        System.out.print("请输入要更改学生的年级>>>>>");
+        int stuGrade = scanner.nextInt();
+        System.out.print("请输入要更改学生的班级>>>>>");
+        int stuClass = scanner.nextInt();
+        System.out.print("请输入要更改学生的专业>>>>>");
+        String stuMajor = scanner.next();
+        System.out.print("请输入要更改学生的学校>>>>>");
+        String stuDepart = scanner.next();
+        PreparedStatement ps = conn.prepareStatement(UPDATE_STUDENT);
+        ps.setString(1, stuId);
+        ps.setString(2, stuName);
+        ps.setString(3, stuSex);
+        ps.setInt(4, stuGrade);
+        ps.setInt(5, stuClass);
+        ps.setString(6, stuMajor);
+        ps.setString(7, stuDepart);
+        if (ps.executeUpdate() > 0) {
+            System.out.println("更改成功！");
+        } else {
+            System.out.println("更改失败！");
+        }
+
+    }
+
+    public static void show() throws Exception {
+        System.out.println("**********查询学生信息***********");
+        System.out.print("请输入要查询学生的学号>>>>>");
+        String stuId = scanner.next();
+        PreparedStatement ps = conn.prepareStatement(SHOW_STUDENT);
+        ps.setString(1, stuId);
+        ResultSet resultSet = ps.executeQuery();
+        System.out.println("**********查询结果***********");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1) + "--" +
+                    resultSet.getString(2) + "--" +
+                    resultSet.getString(3) + "--" +
+                    resultSet.getString(4) + "--" +
+                    resultSet.getString(5) + "--" +
+                    resultSet.getString(6) + "--" +
+                    resultSet.getString(7));
+        }
+    }
+
+    public static void showAll() throws Exception {
+        System.out.println("**********所有学生信息***********");
+        PreparedStatement ps = conn.prepareStatement(SHOWALL_STUDENT);
+        ResultSet resultSet = ps.executeQuery();
+        System.out.println("**********查询结果***********");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1) + "--" +
+                    resultSet.getString(2) + "--" +
+                    resultSet.getString(3) + "--" +
+                    resultSet.getString(4) + "--" +
+                    resultSet.getString(5) + "--" +
+                    resultSet.getString(6) + "--" +
+                    resultSet.getString(7));
+        }
     }
 }
